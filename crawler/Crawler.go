52b173f6d33e5f8f6cba3214e1878e6b00b26543
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2022-11-02 14:43:04 +0100

    feat: read sleep seconds from env

--- crawler/Crawler.go
+++ crawler/Crawler.go
@@ -2,13 +2,35 @@ package crawler
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
+
+	_ "github.com/joho/godotenv/autoload"
 )
+
+func getSleepSeconds() int {
+	sleepSecondsRaw := os.Getenv("SLEEP_SECONDS")
+	sleepSeconds, err := strconv.Atoi(sleepSecondsRaw)
+	if err != nil {
+		defaultSleepSeconds := 60
+		fmt.Printf("SLEEP_SECONDS environment variable is not a valid integer, using a default value of %d seconds.", sleepSeconds)
+		return defaultSleepSeconds
+	}
+	return sleepSeconds
+}
+
+func takeABreak(sleepSeconds int) {
+	fmt.Printf("Taking a break for %d secs...", sleepSeconds)
+	time.Sleep(time.Duration(sleepSeconds) * time.Second)
+}
 
 func Crawler() {
 	Smeg()
 	Digitec()
-	fmt.Println("Taking a break for a minute.")
-	time.Sleep(1 * time.Minute)
+
+	var sleepSeconds = getSleepSeconds()
+	takeABreak(sleepSeconds)
+
 	Crawler()
 }
